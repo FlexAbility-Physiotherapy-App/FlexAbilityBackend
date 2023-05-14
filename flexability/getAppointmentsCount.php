69e36e728acbbc6ac217b31ae9e9e0518f9c10f8
Stavroula Tsoni <stavroula026@gmail.com>
2023-05-14 05:14:33 +0300

    R6 backend done

---
+++ flexability/getAppointmentsCount.php
@@ -0,0 +1,54 @@
+<?php 
+	// Database credentials
+    $host="localhost";
+	$uname="root";
+	$pass="";
+	$dbname="flexability";
+	
+	// Establish a database connection
+	$conn = mysqli_connect($host,$uname,$pass,$dbname) or die("cannot connect");
+	
+	// Check if it's a GET request
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+        // Get parameters date and id
+        $date = isset($_GET['date']) ? $_GET['date'] : '';
+        $physio = isset($_GET['id']) ? intval($_GET['id']) : null;
+
+        // Prepare the SQL statement to fetch the count of appointments
+        $stmt = $conn->prepare("SELECT COUNT(patient_physio.timestamp) 
+                                FROM patient
+                                INNER JOIN patient_physio ON patient.id = patient_physio.patient_id
+                                WHERE DATE(patient_physio.timestamp) = ? AND patient_physio.status = 'accepted' AND patient_physio.physio_id = ?");
+        $stmt->bind_param("si", $date, $physio);
+        
+        // Execute the statement
+        $stmt->execute();
+
+        // Bind the result variable
+        $stmt->bind_result($appointmentCount);
+
+        // Fetch the count of appointments
+        $stmt->fetch();
+
+        // Close the statement
+        $stmt->close();
+
+        // Prepare the response JSON
+        $response = array(
+            'totalAppointments' => $appointmentCount
+        );
+
+        // Convert the response to JSON format
+        $json = json_encode($response);
+
+        // Set the content type to JSON
+        header('Content-Type: application/json');
+
+        // Return the JSON response
+        echo $json;
+    }
+
+    // Close the database connection
+    $conn->close();
+?>
