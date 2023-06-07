05aa008a0cb92c5aa41f760b677ccb9167fb1ee5
Constantinos Vakratsas <c.vakratsas@gmail.com>
2023-06-08 01:24:25 +0300

    added appointments to sample data

--- SampleData.sql
+++ SampleData.sql
@@ -14,11 +14,18 @@ INSERT INTO `patient` (`id`, `name`, `surname`, `phone_number`, `sex`, `amka`) V
 (7, 'Mike', 'Johnson', '5678901234', 'Male', '77777777777');
 
 INSERT INTO `physio` (`id`, `address`, `name`, `owner`, `afm`, `phone_number`) VALUES
-(2, '123 Main Street', 'Physio Care', 'John Doe', '22222222222', '1234567890'),
-(3, '456 Elm Street', 'Physio Wellness', 'Jane Smith', '33333333333', '9876543210'),
-(4, '789 Oak Street', 'Elite Physiotherapy', 'Mike Johnson', '44444444444', '3355115599');
+(2, '123 Main Street', 'Physio Care', 'John Doe', '222222222', '1234567890'),
+(3, '456 Elm Street', 'Physio Wellness', 'Jane Smith', '333333333', '9876543210'),
+(4, '789 Oak Street', 'Elite Physiotherapy', 'Mike Johnson', '444444444', '3355115599');
 
 INSERT INTO `provision` (`id`, `description`, `cost`, `name`) VALUES
 ('P1', 'Basic Check-up', 50, 'Check-up'),
 ('P2', 'X-ray examination', 100, 'X-ray'),
 ('P3', 'Physical therapy session', 80, 'Physical Therapy');
+
+INSERT INTO `patient_physio` (`patient_id`, `physio_id`, `timestamp`, `status`) VALUES
+(5, 2, '2023-06-12 00:12:00', 'accepted'),
+(5, 3, '2023-06-12 00:12:00', 'pending'),
+(6, 3, '2023-06-12 00:12:00', 'accepted'),
+(7, 4, '2023-06-12 00:12:00', 'pending');
+
