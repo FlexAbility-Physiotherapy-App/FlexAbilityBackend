0f5a816a5d50d6817a6c07dd53f00bae3a3d5503
PanosMavv <102112065+PanosMavv@users.noreply.github.com>
2023-06-10 20:37:18 +0300

    Update getRequestedAppointments.php
    * getRequestedAppointments doesnt return old unapproved appointments.

--- flexability/getRequestedAppointments.php
+++ flexability/getRequestedAppointments.php
@@ -8,13 +8,16 @@
         $date = isset($_GET['date']) ? $_GET['date'] : '';
         $physio = isset($_GET['id']) ? intval($_GET['id']) : null;
 
+		// Get the current timestamp
+        $currentTimestamp = time();
+
         // Prepare the SQL statement to fetch appointments with optional limit
 		$stmt = $conn->prepare("SELECT patient.name, patient.surname, patient.amka, patient_physio.timestamp, patient_physio.patient_id  
 				FROM patient
 				INNER JOIN patient_physio ON patient.id = patient_physio.patient_id
-				WHERE DATE(patient_physio.timestamp) = ? AND patient_physio.status = 'pending' AND patient_physio.physio_id = ?
+				WHERE DATE(patient_physio.timestamp) = ? AND patient_physio.status = 'pending' AND patient_physio.physio_id = ? AND UNIX_TIMESTAMP(patient_physio.timestamp) > ?
 				ORDER BY patient_physio.timestamp ASC");
-		$stmt->bind_param("si", $date, $physio);
+		$stmt->bind_param("sii", $date, $physio, $currentTimestamp);
         
         
         // Execute the statement
